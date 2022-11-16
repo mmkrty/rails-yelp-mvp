b7c59b706a35781901fd8dc1b6b80b632e26fd52
haochenliu <mmkrty@gmail.com>
2022-11-16 14:33:18 +0100

    complete basic functions with no style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do |i|
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: category.sample, phone_number: Faker::PhoneNumber.phone_number_with_country_code)
+end
